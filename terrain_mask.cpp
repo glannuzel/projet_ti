e2a63d4f2d99b144dba6be06bf940b6bb8c83419
MENANTEAU Yoann <ymenanteau@anderssen.pedago.ipb.fr>
2018-12-11 10:30:25 +0100

    modif hsv seuillage

--- terrain_mask.cpp
+++ terrain_mask.cpp
@@ -7,29 +7,26 @@
 using namespace cv;
 using namespace std;
 
-#define RED 2
-#define GREEN 1
-#define BLUE 0
+#define H 0
+#define S 1
+#define V 2
 
-Mat to_gray(Mat image){
+/*
+Change a channel value
+ */
+void adjust_channel(Mat &image, const int shift, const int channel){
   int col_size = image.cols;
   int row_size = image.rows;  
-  Mat out = Mat(row_size, col_size, CV_8UC1);
   for (int i = 0; i < row_size; i++){
     for (int j = 0; j < col_size; j++){
-      Vec3b p = image.at<Vec3b>(i,j);
-      double ratio = 1.0;
-      if (p.val[RED]+p.val[BLUE] != 0)
-	ratio = p.val[GREEN]/((p.val[RED]+p.val[BLUE])/2.);
-      else if (p.val[GREEN] == 0)
-	ratio = 0.0;
-      unsigned char & p_out = out.at<unsigned char>(i,j);
-      p_out = ratio * 255;
+      Vec3b &pixel = image.at<Vec3b>(i,j);  
+      if (pixel[channel] + shift <= 255)
+	pixel[channel] += shift;
+      else
+	pixel[channel] = 255;
     }
   }
-  return out;
 }
-
 
 void process (const char * ims){
   Mat image=imread(ims);
@@ -40,15 +37,10 @@ void process (const char * ims){
   if (image.type() != CV_8UC3)
     image.convertTo(image, CV_8UC3);   
   
-  Mat out = to_gray(image);
-  equalizeHist(out,out);
-  // adaptiveThreshold(out, out, 100, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 7, -20);
-  imshow("fds", out);
-  waitKey(0);
   Mat hsv;
   cvtColor(image, hsv, COLOR_BGR2HSV);
-  int sensibility = 60;
-  Scalar lower_green = Scalar(60-sensibility,100,100);
+  int sensibility = 25;
+  Scalar lower_green = Scalar(60-sensibility,0,0);
   Scalar  upper_green =Scalar(60+sensibility,255,255);
   Mat mask;
   inRange(hsv, lower_green, upper_green, mask);
